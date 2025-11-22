918186a8322c0e3b00c2a4a58e1d712e52fae4d4
muhammadzaini213 <oniichanterritory@gmail.com>
2025-11-22 20:34:38 +0800

    add UI

--- src/contoh.py
+++
@@ -1,9 +0,0 @@
-# ================ UI ================ (ui full)
-
-
-# ================ TRIGGER ============== (ini ngambil data dari window)
-def ....
-
-
-# ================ SETTER ============== (ngeset data untuk window)
-def...
